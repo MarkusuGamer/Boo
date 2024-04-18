c2496a9ae0c4f701be6db80ffc4870e4172acb62
Mr. ConQueso <78820389+Mr-ConQueso@users.noreply.github.com>
2024-04-18 17:27:03 +0200

    Settings!

--- Boo/Assets/Scripts/UIController/MainMenuController.cs
+++ Boo/Assets/Scripts/UIController/MainMenuController.cs
@@ -1,15 +1,29 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    // ---- / Serialized Variables / ---- //
+    [SerializeField] private GameObject settingsMenu;
+    
     public void Play()
     {
         SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
     }
 
+    public void SettingsMenu()
+    {
+        settingsMenu.SetActive(true);
+    }
+    
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    private void Start()
+    {
+        settingsMenu.SetActive(false);
     }
 }
